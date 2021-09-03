11a285a0965ee31fe04156f46da345a1a2aad58f
naveendk <naveenkolambage@gmail.com>
2021-09-03 18:04:15 +1200

    updatev1

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const panels= document.querySelectorAll('.panel')
 
-let BtnEle = document.querySelector(".Btn");
-
-
-var  mouseover  = true ;
-
-
 
  
 function removeActiveClassesOnHover(){
@@ -22,16 +16,9 @@ panels.forEach(panel=>{
 }
 
 
-BtnEle.addEventListener("click", () => {
-    mouseover = !mouseover;
-    console.log("i toggled "+ mouseover);
- });
- if(mouseover= false){
-    console.log("effect event is onclick")
     panels.forEach((panel)=>{
-        console.log(effectEvent)
-        console.log(panel)
-       
+         
+    
             panel.addEventListener('click',()=>{
                 removeActiveClassesOnClick()
                panel.classList.add('active')
@@ -40,19 +27,5 @@ BtnEle.addEventListener("click", () => {
     })
 
 
-}
 
  
-if(mouseover=true){
-    console.log("effect event is onhover")
-    panels.forEach((panel)=>{
-        
-        //console.log(panel)
-       
-            panel.addEventListener('mouseover',()=>{
-                removeActiveClassesOnHover()
-               panel.classList.add('active')
-           })
-    })
-
-}
